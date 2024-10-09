fdfb8347c691a16b802f96db27a68459c19ce930
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-10 01:17:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({
+    origin : "https://school-management-xi-nine.vercel.app"
+}))
 
 mongoose
     .connect(process.env.MONGO_URL, {
